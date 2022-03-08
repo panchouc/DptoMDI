4285ee9a7e20e5e3345ee6cb284c15facd5bb1ba
Francisco Solís <fsolisgormaz@uc.cl>
2022-03-08 19:32:57 -0300

    Cambios en main.py

--- src/main.py
+++ src/main.py
@@ -11,12 +11,30 @@ ADEMÁS, SERÍA BUENO REPLICAR LAS MISMAS COLUMNAS EN TODOS LOS WORKSHEETS
 
 """
 
-from functions import generador_archivos, generador_columnas
+from functions import generador_archivos
+from classes import LecturaArchivos
 
 def main():
-    variable = generador_columnas()
+    numero = int(input("¿Que acción desea realizar?\n1) Crear un nuevo archivo\n2) Leer un archivo\n3) Visualizar los gastos no compartidos"))
     
+    if numero == 1:
+        file = input("Ingresa como quieres guardar el archivo, ej: Marzo_2022.xlsx ")
+        prim = int(input("Ingresa el primer día del mes "))
+        fin = int(input("Ingresa el último día del mes "))
+        mes = input("Ingresa el mes, ej: Marzo ")
+        variable = generador_archivos(file, prim, fin, mes)
+    
+    elif numero == 2:
+        nombre = input("Ingrese el nombre del archivo a leer, Ej: Marzo_2022.xlsx ")
+        archivo = LecturaArchivos(nombre)
+        archivo.resumen_total()
+        
+    elif numero == 3:
+        nombre = input("Ingrese el nombre de la persona que quiere revisar sus gastos personales:\nSolo se permite: Francisco o Felipe")
+        archivo = input("Ingrese el nombre del archivo, ej: Marzo_2022.xlsx")
+        lectura = LecturaArchivos(archivo)
+        lectura.gastos_unicos_persona(nombre)
     
 if __name__ == '__main__':
     main()
-    print("HOLA")
+    
